89ad76af029e62f22c312975b97dcabff12270d3
mabdullah <mabdullah2037@gmail.com>
2023-04-26 23:46:07 +0500

    question page completed

---
+++ config/firebase.tsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXKGdne4nTyj83d_bQHzdiOpvtQ1Mxyzc",
+  authDomain: "quiz-223.firebaseapp.com",
+  projectId: "quiz-223",
+  storageBucket: "quiz-223.appspot.com",
+  messagingSenderId: "866905587507",
+  appId: "1:866905587507:web:113a077019b0b5d6a20846",
+  measurementId: "G-E9LG37H6QF"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,103 @@
-import React from 'react'
+import { sendData } from '@/config/firebaseMethod';
+import React, { useState } from 'react'
+import { FloatingLabel, Button } from 'react-bootstrap'
+import Form from 'react-bootstrap/Form';
+import Swal from 'sweetalert2';
 
 function index() {
+  //state
+  const [question, setQuestion] = useState("");
+  const [option01 , setOption01] = useState("");
+  const [option02 , setOption02] = useState("");
+  const [option03 , setOption03] = useState("");
+  const [rightAnswer , setRightAnswer] = useState("");
+
+  //send question function
+  const sendQuestion =  () => {
+    const obj: { 
+      question: string;
+      option01:string;
+      option02:string;
+      option03:string;
+      rightAnswer:string;
+     } = {
+      question,
+      option01,
+      option02,
+      option03,
+      rightAnswer
+    }
+    sendData(obj , "questions")
+    .then(()=>{
+      Swal.fire(
+        '',
+        'Question Added Succesfully!',
+        'success'
+      )
+      setOption01("");
+      setOption02("");
+      setOption03("");
+      setRightAnswer("");
+      setQuestion("");
+    })
+    .catch((err)=>{
+      console.log(err);
+      
+    })
+  }
   return (
-    <div>index</div>
+    <div className='container mt-5'>
+      <h1>Add Questions</h1>
+      <div className='mt-5'>
+        <FloatingLabel controlId="floatingTextarea2" label="Questions">
+          <Form.Control
+            as="textarea"
+            style={{ height: '100px' }}
+            onChange={(e) => setQuestion(e.target.value)}
+            value={question}
+          />
+        </FloatingLabel>
+      </div>
+      <div className="mt-3">
+      <FloatingLabel
+        controlId="floatingTextarea"
+        label="Option-01"
+        className="mb-3"
+      >
+        <Form.Control as="textarea" placeholder="Leave a comment here" onChange={(e)=>setOption01(e.target.value)} value={option01}/>
+      </FloatingLabel>
+      </div>
+      <div className="mt-3">
+      <FloatingLabel
+        controlId="floatingTextarea"
+        label="Option-02"
+        className="mb-3"
+      >
+        <Form.Control as="textarea" placeholder="Leave a comment here" onChange={(e)=>setOption02(e.target.value)} value={option02}/>
+      </FloatingLabel>
+      </div>
+      <div className="mt-3">
+      <FloatingLabel
+        controlId="floatingTextarea"
+        label="Option-03"
+        className="mb-3"
+      >
+        <Form.Control as="textarea" placeholder="Leave a comment here" onChange={(e)=>setOption03(e.target.value)} value={option03}/>
+      </FloatingLabel>
+      </div>
+      <div className="mt-3">
+      <FloatingLabel
+        controlId="floatingTextarea"
+        label="Correct Answer"
+        className="mb-3"
+      >
+        <Form.Control as="textarea" placeholder="Leave a comment here" onChange={(e)=>setRightAnswer(e.target.value)} value={rightAnswer}/>
+      </FloatingLabel>
+      </div>
+      <div className='mt-5'>
+        <Button onClick={sendQuestion} variant="primary">Add Question</Button>
+      </div>
+    </div>
   )
 }
 
